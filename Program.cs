d9ffd2cdc18fd4b59e20c1c22bbd057ffa16d932
Administrator <dyingvoid@gmail.com>
2022-10-02 13:50:07 +0500

    Remembering how this works

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     public static void Main()
     {
+        ExecuteTaskFirst();
+    }
 
+    public static void ExecuteTaskFirst()
+    {
+        var number = Console.ReadLine();
+        Console.WriteLine(number);
     }
 }
