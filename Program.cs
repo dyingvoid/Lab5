7cc924c70149ceb6aad88976272566379ae2f955
Administrator <dyingvoid@gmail.com>
2022-10-02 22:10:16 +0500

    Implemented checking for wrong chars, Abs, started doing Sum.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ class Program
 {
     public static void Main()
     {
-        ExecuteTaskFirst();
+        //ExecuteTaskFirst();
+        ExecuteSecondTask();
     }
 
     public static void ExecuteTaskFirst()
@@ -32,10 +33,56 @@ class Program
         }
     }
 
-    public static bool CheckForIntOverflow(string input)
+    public static void ExecuteSecondTask()
+    {
+        string input = "";
+        bool inputIsInt = true;
+        while(inputIsInt)
+        {
+            input = Console.ReadLine();
+            Console.WriteLine((char)FindSumOfDigitsInNumber(input));
+        }
+    }
+
+    public static int FindSumOfDigitsInNumber(string input)
+    {
+        char minNumber = '0';
+        foreach(var ch in input)
+        {
+            minNumber += ch;
+        }
+        return minNumber - '0' - 24 * input.Length;
+    }
+
+    //Check if set formed from input is subset of checking set
+    //returns true for empty string, check docs to know why
+    public static bool CheckForWrongChars(string input)
+    {
+        //using two hashsets is claimed to be O(n)
+        var inputSet = new HashSet<char>(input);
+        var testingSet = new HashSet<char>(
+            new char[] { '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }
+            );
+        return inputSet.IsProperSubsetOf(testingSet);
+    }
+
+    public static string StringAbs(string number)
+    {
+        string absedString = "";
+        if (number.Length > 0)
+        {
+            if (number[0] == '-')
+                absedString = number.Substring(1, number.Length - 1);
+            else
+                absedString = number;
+        }
+        return absedString;
+    }
+
+    public static bool CheckForIntOverflow(string input, int value)
     {
         bool answer = false;
-        string maxInt = int.MaxValue.ToString();
+        string maxInt = value.ToString();
         if(input.Length > maxInt.Length)
         {
             answer = true;
@@ -50,6 +97,4 @@ class Program
         }
         return answer;
     }
-
-
 }
