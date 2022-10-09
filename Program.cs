5350d33975aee8c536793c3a573d7a73b295ba86
Administrator <dyingvoid@gmail.com>
2022-10-09 19:02:19 +0500

    Completed Round and Cut functions.

--- Program.cs
+++ Program.cs
@@ -1,177 +1,192 @@
 ﻿// Copyright 2022 dyingvoid
 
-using Lab5;
-
-class Program
+namespace Lab5
 {
-    public static void Main()
+    static class Program
     {
-        string option = "";
-        while (option != "q")
+        public static void Main()
         {
-            option = Console.ReadLine();
-            switch (option)
+            string option = "";
+            while (option != "q")
             {
-                case "1":
-                    Console.WriteLine("FirstTask");
-                    ExecuteTaskFirst();
-                    break;
-                case "2":
-                    Console.WriteLine("SecondTask");
-                    ExecuteSecondTask();
-                    break;
-                case "4":
-                    Console.WriteLine("Executing task 4");
-                    ExecuteFourthTask();
-                    break;
-                case "q":
-                    break;
-                default:
-                    Console.WriteLine("Wrong Input.");
-                    break;
+                Console.WriteLine("You have entered menu");
+                option = Console.ReadLine() ?? string.Empty;
+                switch (option)
+                {
+                    case "1":
+                        Console.WriteLine("FirstTask");
+                        ExecuteTaskFirst();
+                        break;
+                    case "2":
+                        Console.WriteLine("SecondTask");
+                        ExecuteSecondTask();
+                        break;
+                    case "4":
+                        Console.WriteLine("Executing task 4");
+                        ExecuteFourthTask();
+                        break;
+                    case "q":
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Input.");
+                        break;
+                }
             }
         }
-    }
 
-    public static void ExecuteTaskFirst()
-    {
-        string input = "";
-        int intToChar = 0;
-        float floatCompare1 = float.NaN;
-        float floatCompare2 = float.NaN;
-        while (input != "q")
+        public static void ExecuteTaskFirst()
         {
-            input = Console.ReadLine();
-            
-            if (!CheckInputToBeIntOrFloat(input))
+            string input = "";
+            int intToChar;
+            float floatCompare1 = float.NaN;
+            float floatCompare2 = float.NaN;
+            while (input != "q")
             {
-                continue;
-            }
+                input = Console.ReadLine() ?? string.Empty;
 
-            if(int.TryParse(input, out intToChar))
-            {
-                Console.WriteLine((char)intToChar);
-            }
-            else if(float.TryParse(input, out floatCompare2))
-            {
-                if (!CheckForIntOverFlowTask1(input))
+                if (!CheckInputToBeIntOrFloat(input))
+                {
                     continue;
-                
-                if (CompareFloats(floatCompare1, floatCompare2, 0.1))
-                    break;
-                floatCompare1 = floatCompare2;
+                }
+
+                if (int.TryParse(input, out intToChar))
+                {
+                    Console.WriteLine((char)intToChar);
+                }
+                else if (float.TryParse(input, out floatCompare2))
+                {
+                    if (!CheckForIntOverFlowTask1(input))
+                        continue;
+
+                    if (CompareFloats(floatCompare1, floatCompare2, 0.1))
+                        break;
+                    floatCompare1 = floatCompare2;
+                }
             }
         }
-    }
 
-    public static bool CheckInputToBeIntOrFloat(string input)
-    {
-        bool answer = true;
-        List<char> checkList = new List<char>()
+        public static bool CheckInputToBeIntOrFloat(string input)
         {
-            '0', '1', '2', '3', '4', '5', '6', '7',
-            '8', '9', '-', '.'
-        };
-        if (string.IsNullOrEmpty(input) || input.Length == 0 || !StringManager.CheckForWrongChars(input, checkList))
-        {
-            Console.WriteLine("Your input contains wrong characters. " +
-                              "Input must be integer or float val");
-            answer = false;
-        }
-
-        return answer;
-    }
-
-    public static bool CheckForIntOverFlowTask1(string input)
-    {
-        bool answer = true;
-        if (!input.Contains('.') && StringManager.IsLess(input, int.MaxValue.ToString()))
-        {
-            Console.WriteLine($"You entered too big number, integer value must fit in {int.MinValue} : " +
-                              $"{int.MaxValue} range.");
-            answer = false;
-        }
-
-        return answer;
-    }
-
-    public static bool CompareFloats(float first, float second, double eps)
-    {
-        bool answer = false;
-        float absFirst = Math.Abs(first);
-        float absSecond = Math.Abs(second);
-        float absDif = Math.Abs(absFirst - absSecond);
-        if (absDif <= 0 + eps && absDif >= 0 - eps)
-            answer = true;
-        return answer;
-    }
-
-    public static void ExecuteSecondTask()
-    {
-        string input = "";
-        bool inputIsInt = true;
-        while(inputIsInt)
-        {
-            input = Console.ReadLine();
-            if (CheckInputTask2(input))
-                Console.WriteLine(StringManager.FindSumOfDigitsInNumber(input));
-            else
-                Console.WriteLine("Invalid input - input must be integer number.");
-        }
-    }
-
-    public static bool CheckInputTask2(string input)
-    {
-        bool answer = input.Length > 0 &&
-            StringManager.CheckForWrongChars(input);
-        if (answer)
-        {
-            if (StringManager.IsNegative(input))
-                answer = !StringManager.IsLess(input, int.MinValue.ToString());
-            else
-                answer = !StringManager.IsLess(input, int.MaxValue.ToString());
-        }
-        return answer;
-    }
-
-    public static void ExecuteFourthTask()
-    {
-        Console.WriteLine("Enter size of array as integer number.");
-        while (true)
-        {
-            string inputArrStr = Console.ReadLine();
-            Console.WriteLine("Input passed.");
-            var inputArr = new List<int>();
-            if (CheckInputTask4(inputArrStr, ref inputArr))
+            bool answer = true;
+            if (string.IsNullOrEmpty(input) || input.Length == 0 ||
+                !(StringManager.IsInt(input) || StringManager.IsFloat(input)))
             {
-                inputArr.ForEach(Console.WriteLine);
+                Console.WriteLine("Your input contains wrong characters. " +
+                                  "Input must be integer or float val");
+                answer = false;
+            }
+
+            return answer;
+        }
+
+        public static bool CheckForIntOverFlowTask1(string input)
+        {
+            bool answer = true;
+            if (!input.Contains('.') && StringManager.IsBigger(input, int.MaxValue.ToString()))
+            {
+                Console.WriteLine($"You entered too big number, integer value must fit in {int.MinValue} : " +
+                                  $"{int.MaxValue} range.");
+                answer = false;
+            }
+
+            return answer;
+        }
+
+        public static bool CompareFloats(float first, float second, double eps)
+        {
+            bool answer = false;
+            float absDif = Math.Abs(first - second);
+            if (absDif <= 0 + eps && absDif >= 0 - eps)
+                answer = true;
+            return answer;
+        }
+
+        public static void ExecuteSecondTask()
+        {
+            string input;
+            bool inputIsInt = true;
+            while (inputIsInt)
+            {
+                input = Console.ReadLine() ?? string.Empty;
+                if (CheckInputTask2(input))
+                    Console.WriteLine(StringManager.FindSumOfDigitsInNumber(input));
+                else
+                    Console.WriteLine("Invalid input - input must be integer number.");
             }
         }
-    }
 
-    public static bool CheckInputTask4(string input, ref List<int> inputArr)
-    {
-        bool answer;
-        if (!string.IsNullOrEmpty(input))
+        public static bool CheckInputTask2(string input)
         {
-            List<string> inputArrStr = new List<string>(input.Split(" "));
-            answer = inputArrStr.Any(num => CheckInputToBeIntOrFloat(num) && 
-                                         CheckForIntOverFlowTask1(num));
+            bool answer = input.Length > 0 &&
+                          StringManager.IsInt(input);
             if (answer)
-                inputArr = inputArrStr.Select(int.Parse).ToList();
-            else 
-                Console.WriteLine("At least one value is not a number.");
-        }
-        else
-        {
-            answer = false;
-            Console.WriteLine("Null or empty input.");
+            {
+                if (StringManager.IsNegative(input))
+                    answer = !StringManager.IsBigger(input, int.MinValue.ToString());
+                else
+                    answer = !StringManager.IsBigger(input, int.MaxValue.ToString());
+            }
+            return answer;
         }
 
-        return answer;
+        public static void ExecuteFourthTask()
+        {
+            Console.WriteLine("Enter array with spaces between numbers.");
+            while (true)
+            {
+                string inputArrStr = Console.ReadLine() ?? string.Empty;
+                Console.WriteLine("Input passed.");
+                var inputArr = new List<string>();
+                if (CheckInputTask4(inputArrStr, ref inputArr))
+                {
+                    var outputArr = OperateInputArray(inputArr);
+                    outputArr.ForEach(Console.WriteLine);
+                }
+            }
+        }
+
+        public static List<string> OperateInputArray(List<string> inputArr)
+        {
+            var outputArr = new List<string>();
+            foreach (var num in inputArr)
+            {
+                if (StringManager.IsInt(num) && !StringManager.IsNegative(num))
+                {
+                    int res = Enumerable.Range(1, int.Parse(num)).Aggregate(1, (p, item) => p * item);
+                    outputArr.Add(res.ToString());
+                }
+                else if (StringManager.IsFloat(num))
+                {
+                    string res = StringManager.RoundAndCut(num, 2);
+                    outputArr.Add(res);
+                }
+                else
+                {
+                    outputArr.Add(num);
+                }
+            }
+            return outputArr;
+        }
+
+        public static bool CheckInputTask4(string input, ref List<string> inputArr)
+        {
+            bool answer;
+            if (!string.IsNullOrEmpty(input))
+            {
+                List<string> inputArrStr = new List<string>(input.Split(" "));
+                answer = inputArrStr.Any(num => CheckInputToBeIntOrFloat(num) &&
+                                                CheckForIntOverFlowTask1(num));
+                if (answer)
+                    inputArr = inputArrStr;
+                else
+                    Console.WriteLine("At least one value is not a number.");
+            }
+            else
+            {
+                answer = false;
+                Console.WriteLine("Null or empty input.");
+            }
+            return answer;
+        }
     }
-    
-    
-    
-    
 }
--- StringManager.cs
+++ StringManager.cs
@@ -25,17 +25,15 @@
         /// <summary>
         /// Checks if set formed from input is subset of checking set,
         /// to be precious if string contains nums
-        /// or '-' in the right place(cant be [1] or further)
+        /// and '-' or '.' in the right place(cant be [1] or further)
         /// </summary>
-        /// <param name="input">string number</param>
+        /// <param name="input">number in string form</param>
+        /// <param name="additionalSymbols">List of checking chars</param>
         /// <returns>true for right or empty input, false otherwise</returns>
-        public static bool CheckForWrongChars(string input, List<char> additionalSymbols = null)
+        public static bool CheckForWrongChars(string input, List<char> additionalSymbols)
         {
             bool answer;
-            additionalSymbols ??= new List<char>() {
-                '0', '1', '2', '3', '4', '5', '6', '7',
-                '8', '9', '-'
-            };
+            additionalSymbols ??= new List<char>() { };
             //using two hashsets is claimed to be O(n)
             var inputSet = new HashSet<char>(input);
             var testingSet = new HashSet<char>(additionalSymbols);
@@ -43,15 +41,55 @@
             answer = inputSet.IsProperSubsetOf(testingSet) &&
                      input.Substring(1, input.Length - 1).IndexOf('-') == -1;
             if (answer && input.Length == 1)
+            {
                 answer = input[0] != '-';
+            }
+
             return answer;
         }
 
         /// <summary>
-        /// 
+        /// Uses StringManager.CheckForWrongChars()
+        /// with possible integer chars in additionalSymbols list
+        /// </summary>
+        /// <param name="input">number in string form</param>
+        /// <returns>true if integer, false otherwise</returns>
+        public static bool IsInt(string input)
+        {
+            var checkList = new List<char> {
+                '0', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', '-'
+            };
+            bool answer = CheckForWrongChars(input, checkList);
+            return answer;
+        }
+        
+        /// <summary>
+        /// Uses StringManager.CheckForWrongChars()
+        /// with possible float chars in additionalSymbols list
+        /// </summary>
+        /// <param name="input">number in string form</param>
+        /// <returns>true if float, false otherwise</returns>
+        public static bool IsFloat(string input)
+        {
+            var checkList = new List<char>(){
+                '0', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', '-', '.'
+            };
+            int pointIdx = input.IndexOf('.');
+            
+            bool answer = CheckForWrongChars(input, checkList) &&
+                          input.Count(x=> x =='.') == 1 &&
+                          pointIdx != 0 &&
+                          input.Length - pointIdx - 1 > 0 &&
+                          IsInt(input[pointIdx - 1].ToString());
+            return answer;
+        }
+
+        /// <summary>
         /// </summary>
         /// <param name="number"></param>
-        /// <returns>string without '-' in [0] place</returns>
+        /// <returns>string without '-' at [0] index</returns>
         public static string StringAbs(string number)
         {
             string absedString = "";
@@ -69,12 +107,12 @@
         }
 
         /// <summary>
-        /// Checks input to be less than value
+        /// Checks input to be bigger than value
         /// </summary>
         /// <param name="input"></param>
         /// <param name="value"></param>
         /// <returns>true if input is bigger than value, false otherwise</returns>
-        public static bool IsLess(string input, string value)
+        public static bool IsBigger(string input, string value)
         {
             bool answer = false;
             if (input.Length > value.Length)
@@ -90,6 +128,50 @@
                 }
             }
             return answer;
+        }
+
+        public static string RoundAndCut(string number, int midPointCut)
+        {
+            number = Cut(Round(number, midPointCut));
+            return number;
+        }
+        
+        public static string Cut(string number)
+        {
+            string cutNumber;
+            int pointIndex = number.IndexOf('.');
+            cutNumber = number.Substring(pointIndex + 1, number.Length - pointIndex - 1);
+
+            int startingDigitIndex = 0;
+            foreach (var digit in cutNumber)
+            {
+                if (digit != 0)
+                    break;
+                startingDigitIndex += 1;
+            }
+            return cutNumber.Substring(startingDigitIndex + 1, cutNumber.Length - startingDigitIndex - 1);
+        }
+        
+        /// <summary>
+        /// Round a string number at midPoint to the nearest value
+        /// </summary>
+        /// <param name="number">string number</param>
+        /// <param name="midPointRounding">point of rounding</param>
+        /// <returns>Rounded value</returns>
+        public static string Round(string number, int midPointRounding)
+        {
+            string roundedNumber;
+            int pointIndex = number.IndexOf('.');
+            char[] safeNumberArr = new char[number.Length + midPointRounding];
+            for (int i = 0; i < number.Length; ++i)
+                safeNumberArr[i] = number[i];
+
+            if (safeNumberArr[pointIndex + midPointRounding + 1] >= '5')
+            {
+                safeNumberArr[pointIndex + midPointRounding] += (char)('1' - '0');
+            }
+            roundedNumber = new string(safeNumberArr).Substring(0, pointIndex + midPointRounding + 1);
+            return roundedNumber;
         }
     }
 }
